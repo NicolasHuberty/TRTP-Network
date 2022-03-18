b57e94c51b539c85c34fc010d0686906a1c5a430
Nicolas <huberty.nicolas@hotmail.com>
2022-03-18 18:20:44 +0100

    bind not working

--- SendAndReceiveData/create_socket.c
+++ SendAndReceiveData/create_socket.c
@@ -15,8 +15,8 @@ int create_socket(struct sockaddr_in6 *source_addr,int src_port,struct sockaddr_
     if(sock == -1) perror("Socket error");
     source_addr->sin6_port = htons(src_port);
     if(source_addr != NULL){
-        int bindf = bind(sock, (struct sockaddr *) source_addr, sizeof(struct sockaddr_in6));
-        if(bindf == -1) perror("Bind error: ");return -1;
+        int bindf = bind(sock, (struct sockaddr *) source_addr, sizeof(struct sockaddr));
+        if(bindf == -1) perror("Bind error: ");return -100;
     }
     dest_addr->sin6_family = AF_INET6;
     dest_addr->sin6_port = htons(dst_port);
@@ -24,5 +24,6 @@ int create_socket(struct sockaddr_in6 *source_addr,int src_port,struct sockaddr_
         int connectfd = connect(sock, (struct sockaddr *) dest_addr, sizeof(dest_addr));
         if(connectfd == 1) perror("Connect error: ");
     }
-    return  0;
+    printf("Socket send: %d\n",sock);
+    return  sock;
 }
